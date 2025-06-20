170077848e3255c65ff78f5d9e6c56368337fa4a
William Jhonata <williamjhonata7@outlook.com>
2025-06-20 19:14:12 -0300

    exec

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62
+console.log(`Sua velocidade é de ${vel}KM/h`)
+if (vel > 60) {
+    console.log('Você foi multado!')
+}
+console.log('Dirija com segurança!')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'França'
+console.log()
+if (pais == 'Brasil') {
+    console.log(`você é brasileiro(a)`)
+} else {
+    console.log(`você é estrangeiro(a)!`)
+}
