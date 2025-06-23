2ce19959d0e0baffae682e849a4a93d3e6aa3444
William Jhonata <williamjhonata7@outlook.com>
2025-06-23 19:09:46 -0300

    2 exe

--- exercicios/aula12/ex13.js
+++ exercicios/aula12/ex13.js
@@ -1,2 +1,9 @@
 var hora = new Date()
-var agora = hora.get
+var agora = hora.getHours
+if(agora < 12) {
+    console.log('Bom dia!')
+} else if(agora <= 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
